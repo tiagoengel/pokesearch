a6c8838bfee2a6fbcfaf4abbac97ef84cae9e1d8
Tiago Engel <tiagohngl@gmail.com>
2016-09-25 18:03:53 -0300

    Add clj-http dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.8"]
-                 [org.clojure/tools.logging "0.3.1"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [clj-http "2.3.0"]]
   :ring {:handler pokesearch.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
