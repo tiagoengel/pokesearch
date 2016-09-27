2a9966f7261ceef4656fd36272646a36f90b9159
Tiago Engel <tiagohngl@gmail.com>
2016-09-27 20:14:26 -0300

    Prepare the project to deploying it to heroku

--- project.clj
+++ project.clj
@@ -1,14 +1,23 @@
 (defproject pokesearch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A REST api for finding pokémons"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.8"]
                  [slingshot "0.12.2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/core.cache "0.6.4"]
-                 [clj-http "2.3.0"]]
+                 [clj-http "2.3.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]]
+
   :ring {:handler pokesearch.handler/app}
+  :main pokesearch.handler
+
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
   :uberjar-name "server.jar"
-  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+  :profiles {:uberjar {:aot :all
+                       :env {:production true}}
+             :dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [ring/ring-mock "0.3.0"]
                                   [clj-http-fake "1.0.2"]]
                    :plugins [[ikitommi/lein-ring "0.9.8-FIX"]
--- src/pokesearch/handler.clj
+++ src/pokesearch/handler.clj
@@ -1,7 +1,11 @@
 (ns pokesearch.handler
+  (:gen-class)
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
             [ring.util.response :refer [redirect]]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]
+            [clojure.tools.logging :as log]
             [pokesearch.exception :as ex]
             [pokesearch.jsonapi :as jsonapi]
             [pokesearch.core :refer :all])
@@ -28,3 +32,9 @@
         :return Pokemon
         :summary "find a pokemon by its name"
         (ok (find-pokemon-by-name name))))))
+
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 3000))]
+    (log/info (str "Starting the server in " (or (and (env :production) "production") "development")))
+    (jetty/run-jetty app {:port port :join? false})))
